c513fa71420acccf3bc0c25ea792f3af07a89cb8
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2024-10-17 16:21:14 +0200

    jquery-assignment 1

---
+++ script.js
@@ -0,0 +1,38 @@
+$("#navbar").mouseenter(function () {
+  $(this).css({
+    background: "pink",
+  });
+});
+
+let $this = $(this);
+setTimeout(function () {
+  $this.css(
+    {
+      background: "blue",
+    },
+    3000
+  );
+});
+
+$("button").click(function () {
+  alert("Wow,this button works");
+});
+
+$("img").click(function () {
+  $(this).fadeOut();
+});
+
+$(".card").click(function () {
+  $(this).css({
+    transform: "scale(1.05)",
+  });
+});
+
+$("h1").mouseenter(function () {
+  $(this).css({
+    transform: "scale(2.05)",
+    border: "1px solid black",
+  });
+});
+
+
